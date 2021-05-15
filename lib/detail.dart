a93bfd78d23db316ad42fcbb92a3183f9ed57d5e
captain-blue210 <captain.blue210@gmail.com>
2021-05-15 13:03:50 +0900

    feat : display images

--- lib/detail.dart
+++ lib/detail.dart
@@ -12,9 +12,27 @@ class WordDetailPage extends StatelessWidget {
       ),
       body: Center(
         child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[Text("詳細"), Text(wordPair)],
+          color: Colors.blue,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("詳細"),
+                Text(wordPair),
+                Text("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"),
+                Container(
+                    width: 100,
+                    height: 100,
+                    child: Image.network(
+                        "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg")),
+                Container(
+                    width: 100,
+                    height: 100,
+                    child: Image.asset("images/myicon2.jpg")),
+                Icon(Icons.home)
+              ],
+            ),
           ),
         ),
       ),
